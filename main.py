e610a1cee162dc1c7f58d11045b7982a44459f59
AYUSH SHARMA <120780215+AyushSharma-007@users.noreply.github.com>
2024-06-03 00:13:00 +0530

    main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ config = {
   "databaseURL": "https://assignment-efe1b-default-rtdb.firebaseio.com/",
   "storageBucket": "assignment-efe1b-default-rtdb.appspot.com"
 }
-
+db = firebase.database()
+db.child("users").child("Morty")
+data = {"name": "Mortimer 'Morty' Smith"}
+db.child("users").push(data)
 firebase = pyrebase.initialize_app(config)
 // used to insert the database in it 
